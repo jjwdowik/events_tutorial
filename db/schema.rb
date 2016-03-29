1ce0d21843dc9f5def515f745614328dfb1b8b72
jjwdowik <jjwdowik@umich.edu>
2016-03-28 23:00:43 -0400

    add fields to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329021003) do
+ActiveRecord::Schema.define(version: 20160329024858) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160329021003) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "starts_at"
+    t.text     "description"
   end
 
 end
