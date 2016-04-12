4daf05e4191d89cf859445e96678e6f44121a2f6
jjwdowik <jjwdowik@umich.edu>
2016-04-11 23:41:16 -0400

    route linking

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,8 @@ class EventsController < ApplicationController
 		@events = Event.all
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Events::Application.routes.draw do
-
+  root "events#index"
   get "events" => "events#index"
+  get "events/:id" => "events#show", as: "event"
 
 end
